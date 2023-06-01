c228d7b0d1be9ff09912d09ad3ae90bb7a832067
AntoniaJost <antonia.jost@web.de>
2023-06-01 11:39:46 +0200

    running script creating plots for every day and buoy automatically

--- analysis_subdaily.R
+++ analysis_subdaily.R
@@ -6,6 +6,7 @@
 
 # if desired, clear workspace
 rm(list=ls())
+load("~/Downloads/sidfex.evaluate.R")
 
 # load packages (if not already loaded)
 require(spheRlab)
@@ -30,7 +31,9 @@ gid = "eccc001"
 mid = "giops"
 # if you don't want any date specification, please remove parameters iy and doy completely from subind.fcst (doesn't work with empty string)
 iy = "2021"
-idoy = "50"
+idoy = "50" 
+plot = FALSE # if plots are wanted (might take quite long)
+matrix = TRUE # if matrix with errors is wanted
 
 # update fcst and obs data (set TRUE if desired)
 if (FALSE) {
@@ -91,14 +94,18 @@ for (i.tid in 1:length(tids)) {
     
     # loop over every of the sub data sets
     for (i.sub in 1:length(obs_subdiv)) {
+      if (i == 49 && i.sub == 9 || i == 234 && i.sub == 10) {
+        i.sub=- i.sub + 1
+        next  # skip error plot (too close to north pole, creating NaNs and Infs)
+      }
       # bringing subdivided fcst and obs datasets back to "normal" format to use SIDFEx and spherelab functions
       obs_subdiv_temp = obs # copy normal obs for usual format
-      obs_subdiv_temp$res.list = list(obs_subdiv_temp$res.list[[1]])
+      obs_subdiv_temp$res.list = list(obs_subdiv_temp$res.list[[i]])
       obs_subdiv_temp$res.list[[1]]$data = NULL # remove any data
       obs_subdiv_temp$res.list[[1]]$data = as.data.frame(append(obs_subdiv_temp$res.list[[1]]$data, obs_subdiv[[i.sub]])) # "overwrite" with reduced dataset of just one day leadtime
       # same for fcst
-      fcst_subdiv_temp = fcst
-      fcst_subdiv_temp$res.list = list(fcst_subdiv_temp$res.list[[1]])
+      fcst_subdiv_temp = fcst # hier beginnt Fehler! Darf nicht einfach res.list[[1]] Sachen (vor Data) von fcst uebernehmen, denn das wird immer das Alte sein
+      fcst_subdiv_temp$res.list = list(fcst_subdiv_temp$res.list[[i]]) # hier mit fcst$res.list[[i]] ueberschreiben?
       fcst_subdiv_temp$res.list[[1]]$data = NULL
       fcst_subdiv_temp$res.list[[1]]$data = as.data.frame(append(fcst_subdiv_temp$res.list[[1]]$data, fcst_subdiv[[i.sub]]))
     
@@ -143,84 +150,104 @@ for (i.tid in 1:length(tids)) {
       # create a "reference" forecast that drifts linearly from 0h to 24h (and beyond)
       fcst.2points = fcst.adj
       fcst.2points$res.list[[1]]$data = fcst.2points$res.list[[1]]$data[c(1, nrow(fcst.2points$res.list[[1]]$data)), ] # get first and last data point of the day (= 0-23.5.. h)
-      fcst.lin = sidfex.remaptime.fcst(fcst = fcst.2points, newtime.DaysLeadTime = fcst.adj$res.list[[1]]$data$DaysLeadTime, extrapolate = TRUE) # create 48 corresponding data points in between for better comparison
+      
+      var = sidfex.ydoy2reltime(Year = fcst.2points$res.list[[1]]$data$Year[1], DayOfYear = fcst.2points$res.list[[1]]$data$DayOfYear[1], RefYear = fcst.2points$res.list[[1]]$InitYear, RefDayOfYear = fcst.2points$res.list[[1]]$InitDayOfYear) # variable to store difference (in days) between InitDOY and DOY
+      # some printing for error hunting
+      print(paste0("i:", i))
+      print(paste0("i.sub:", i.sub))
+      print(var)
+      print(paste0("DOY:", fcst.2points$res.list[[1]]$data$DayOfYear[1]))
+      print(paste0("DOY_Init:", fcst.2points$res.list[[1]]$InitDayOfYear))
+      
+      fcst.lin = sidfex.remaptime.fcst(fcst = fcst.2points, newtime.DaysLeadTime = (fcst.adj$res.list[[1]]$data$DaysLeadTime), extrapolate = TRUE) # create 48 corresponding data points in between for better comparison
+      # important! newtime.DaysLeadTime NEEDS parameter var because it always takes Init Value of fcst, not res.list[[1]]$data value!!
       
       # evaluation
       fcst.adj.eval = sidfex.evaluate(obs = obs.adj, fcst = fcst.adj, do.speedangle = TRUE, verbose=FALSE)
       fcst.lin.eval = sidfex.evaluate(obs = obs.adj, fcst = fcst.lin, do.speedangle = TRUE, verbose=FALSE)
       
       # some plotting
-      plot.title = paste0(fcst$res.list[[i]]$TargetID, "_", fcst$res.list[[i]]$GroupID, "_", fcst$res.list[[i]]$MethodID, "_", fcst$res.list[[i]]$InitYear, "-", fcst$res.list[[i]]$InitDayOfYear, "_ld:", i.sub)
-      if (!file.exists(file.path(wd, plot.title))) {
-        png(filename = plot.title, height = 21, width = 29.7, units = "cm", res = 300) # save in DinA4 format (open png)
-      } else {
-        print(paste0("File '", plot.title, "' already exists. Skipped it.")) # skip if file already exists
-        next
+      if (plot = TRUE) {
+        
+        plot.title = paste0(fcst$res.list[[i]]$TargetID, "_", fcst$res.list[[i]]$GroupID, "_", fcst$res.list[[i]]$MethodID, "_", fcst$res.list[[i]]$InitYear, "-", fcst$res.list[[i]]$InitDayOfYear, "_ld:", i.sub, ".png")
+        if (!file.exists(file.path(wd, plot.title))) {
+          png(filename = plot.title, height = 21, width = 29.7, units = "cm", res = 300) # save in DinA4 format (open png)
+        } else {
+          print(paste0("File '", plot.title, "' already exists. Skipped it.")) # skip if file already exists
+          next
+        }
+        
+        par(mfrow=c(2,2))
+        
+        # calculating ylim for plots
+        range_calc = function(eval_value) {
+          ylim = c()
+          is_naN_inf = is.na(eval_value) | is.nan(eval_value) | is.infinite(eval_value)
+          ylim = range(eval_value[!is_naN_inf])
+          return(ylim)
+        }
+        
+        ylim.lin.speed = range_calc(fcst.lin.eval$res.list[[1]]$ens.mean.relspeed)
+        ylim.lin.angle = range_calc(fcst.lin.eval$res.list[[1]]$ens.mean.angle)
+        ylim.lin.gc = range_calc(fcst.lin.eval$res.list[[1]]$ens.mean.gc.dist)
+        ylim.adj.speed = range_calc(fcst.adj.eval$res.list[[1]]$ens.mean.relspeed)
+        ylim.adj.angle = range_calc(fcst.adj.eval$res.list[[1]]$ens.mean.angle)
+        ylim.adj.gc = range_calc(fcst.adj.eval$res.list[[1]]$ens.mean.gc.dist)
+        
+        range_gc = range(ylim.lin.gc, ylim.adj.gc)
+        range_speed = range(ylim.lin.speed, ylim.adj.speed)
+        range_angle = range(ylim.lin.angle, ylim.adj.angle)
+        
+        # plot trajectories in x-y-space
+        fcst.adj.xyz = sl.lonlat2xyz(lon=fcst.adj$res.list[[1]]$data$Lon, lat=fcst.adj$res.list[[1]]$data$Lat)
+        fcst.lin.xyz = sl.lonlat2xyz(lon=fcst.lin$res.list[[1]]$data$Lon, lat=fcst.lin$res.list[[1]]$data$Lat)
+        obs.adj.xyz = sl.lonlat2xyz(lon=obs.adj$res.list[[1]]$data$Lon, lat=obs.adj$res.list[[1]]$data$Lat)
+        xyz.1d = sl.lonlat2xyz(lon=obs.rot.1d$Lon, lat=obs.rot.1d$Lat)  # position after 1 day, so that the plot can be scaled reasonably
+        dist.1d = sqrt(xyz.1d$x^2 + xyz.1d$y^2)
+        
+        plot(NA,xlim=c(-0.3,1)*dist.1d*1.1, ylim=c(-1,1)*dist.1d*1.1,xlab="x / Earth radius",ylab="y / Earth radius")
+        #abline(h=0,v=0,col="grey",lty=3)
+        points(x=obs.adj.xyz$x, y=obs.adj.xyz$y, col="black",cex=0.3)
+        points(x=fcst.lin.xyz$x, y=fcst.lin.xyz$y, col="grey",cex=0.3)
+        points(x=fcst.adj.xyz$x, y=fcst.adj.xyz$y, col="red",cex=0.3)
+        points(x=dist.1d, y=0, pch="+", col="orange",cex=2)
+        legend("topleft", c("obs adjusted", "fcst linear", "fcst adjusted", "1 day"), col = c("black","grey","red","orange"), pch = c(1,1,1,3), bty = "n", cex = 0.7)
+        
+        # plot evaluation results
+        # great-circle distance
+        plot(x=fcst.lin$res.list[[1]]$data$DaysLeadTime, y=fcst.lin.eval$res.list[[1]]$ens.mean.gc.dist, xlab="days lead time", ylab = "great-circle distance / m",
+             xlim=c(i.sub-1.05,i.sub+0.05), ylim = range_gc, col = "grey")
+        abline(h=0,v=i.sub,col="grey",lty=3)
+        points(x=fcst.adj$res.list[[1]]$data$DaysLeadTime, y=fcst.adj.eval$res.list[[1]]$ens.mean.gc.dist, col="red")
+        legend("topleft", c("Error betw. obs & daily fcst", "Error betw. obs & hh fcst"), col = c("grey", "red"), pch = c(1,1), bty = "n", cex = 0.7)
+        
+        # relative speed
+        plot(x=fcst.lin$res.list[[1]]$data$DaysLeadTime, y=fcst.lin.eval$res.list[[1]]$ens.mean.relspeed, xlab="days lead time", ylab = "relative speed",
+             xlim=c(i.sub-1.05,i.sub+0.05), ylim=range_speed, col="grey")
+        points(x=fcst.adj$res.list[[1]]$data$DaysLeadTime, y=fcst.adj.eval$res.list[[1]]$ens.mean.relspeed, col="red")
+        abline(h=1,v=i.sub,col="grey",lty=3)
+        
+        # relative angle
+        plot(x=fcst.lin$res.list[[1]]$data$DaysLeadTime, y=fcst.lin.eval$res.list[[1]]$ens.mean.angle, xlab="days lead time", ylab = "relative angle / degree left",
+             xlim=c(i.sub-1.05,i.sub+0.05), ylim=range_angle, col="grey")
+        abline(h=0,v=i.sub,col="grey",lty=3)
+        points(x=fcst.adj$res.list[[1]]$data$DaysLeadTime, y=fcst.adj.eval$res.list[[1]]$ens.mean.angle, col="red")
+        
+        # add title
+        par(oma = c(0, 0, 3, 0))
+        title(main=paste0("Analysis subdaily resolution \n ", fcst$res.list[[1]]$TargetID, "_", fcst$res.list[[1]]$GroupID, "_", fcst$res.list[[1]]$MethodID, "_", fcst$res.list[[1]]$InitYear, "-", fcst$res.list[[1]]$InitDayOfYear), outer = T)
+        
+        dev.off()
+      } # end if-clause for plotting
+      
+      # some statistics
+      if(matrix = TRUE) {
+        
       }
       
-      par(mfrow=c(2,2))
-      
-      # calculating ylim for plots
-      range_calc = function(eval_value) {
-        ylim = c()
-        is_naN_inf = is.na(eval_value) | is.nan(eval_value) | is.infinite(eval_value)
-        ylim = range(eval_value[!is_naN_inf])
-        return(ylim)
-      }
-      
-      ylim.lin.speed = range_calc(fcst.lin.eval$res.list[[1]]$ens.mean.relspeed)
-      ylim.lin.angle = range_calc(fcst.lin.eval$res.list[[1]]$ens.mean.angle)
-      ylim.lin.gc = range_calc(fcst.lin.eval$res.list[[1]]$ens.mean.gc.dist)
-      ylim.adj.speed = range_calc(fcst.adj.eval$res.list[[1]]$ens.mean.relspeed)
-      ylim.adj.angle = range_calc(fcst.adj.eval$res.list[[1]]$ens.mean.angle)
-      ylim.adj.gc = range_calc(fcst.adj.eval$res.list[[1]]$ens.mean.gc.dist)
-      
-      range_gc = range(ylim.lin.gc, ylim.adj.gc)
-      range_speed = range(ylim.lin.speed, ylim.adj.speed)
-      range_angle = range(ylim.lin.angle, ylim.adj.angle)
-      
-      # plot trajectories in x-y-space
-      fcst.adj.xyz = sl.lonlat2xyz(lon=fcst.adj$res.list[[1]]$data$Lon, lat=fcst.adj$res.list[[1]]$data$Lat)
-      fcst.lin.xyz = sl.lonlat2xyz(lon=fcst.lin$res.list[[1]]$data$Lon, lat=fcst.lin$res.list[[1]]$data$Lat)
-      obs.adj.xyz = sl.lonlat2xyz(lon=obs.adj$res.list[[1]]$data$Lon, lat=obs.adj$res.list[[1]]$data$Lat)
-      xyz.1d = sl.lonlat2xyz(lon=obs.rot.1d$Lon, lat=obs.rot.1d$Lat)  # position after 1 day, so that the plot can be scaled reasonably
-      dist.1d = sqrt(xyz.1d$x^2 + xyz.1d$y^2)
-      
-      plot(NA,xlim=c(-0.3,1)*dist.1d*1.1, ylim=c(-1,1)*dist.1d*1.1,xlab="x / Earth radius",ylab="y / Earth radius")
-      #abline(h=0,v=0,col="grey",lty=3)
-      points(x=obs.adj.xyz$x, y=obs.adj.xyz$y, col="black",cex=0.3)
-      points(x=fcst.lin.xyz$x, y=fcst.lin.xyz$y, col="grey",cex=0.3)
-      points(x=fcst.adj.xyz$x, y=fcst.adj.xyz$y, col="red",cex=0.3)
-      points(x=dist.1d, y=0, pch="+", col="orange",cex=2)
-      legend("topleft", c("obs adjusted", "fcst linear", "fcst adjusted", "1 day"), col = c("black","grey","red","orange"), pch = c(1,1,1,3), bty = "n", cex = 0.7)
-      
-      # plot evaluation results
-      # great-circle distance
-      plot(x=fcst.lin$res.list[[1]]$data$DaysLeadTime, y=fcst.lin.eval$res.list[[1]]$ens.mean.gc.dist, xlab="days lead time", ylab = "great-circle distance / m",
-           xlim=c(i.sub-1.05,i.sub+0.05), ylim = range_gc, col = "grey")
-      abline(h=0,v=i.sub,col="grey",lty=3)
-      points(x=fcst.adj$res.list[[1]]$data$DaysLeadTime, y=fcst.adj.eval$res.list[[1]]$ens.mean.gc.dist, col="red")
-      legend("topleft", c("Error betw. obs & daily fcst", "Error betw. obs & hh fcst"), col = c("grey", "red"), pch = c(1,1), bty = "n", cex = 0.7)
-      
-      # relative speed
-      plot(x=fcst.lin$res.list[[1]]$data$DaysLeadTime, y=fcst.lin.eval$res.list[[1]]$ens.mean.relspeed, xlab="days lead time", ylab = "relative speed",
-           xlim=c(i.sub-1.05,i.sub+0.05), ylim=range_speed, col="grey")
-      points(x=fcst.adj$res.list[[1]]$data$DaysLeadTime, y=fcst.adj.eval$res.list[[1]]$ens.mean.relspeed, col="red")
-      abline(h=1,v=i.sub,col="grey",lty=3)
-      
-      # relative angle
-      plot(x=fcst.lin$res.list[[1]]$data$DaysLeadTime, y=fcst.lin.eval$res.list[[1]]$ens.mean.angle, xlab="days lead time", ylab = "relative angle / degree left",
-           xlim=c(i.sub-1.05,i.sub+0.05), ylim=range_angle, col="grey")
-      abline(h=0,v=i.sub,col="grey",lty=3)
-      points(x=fcst.adj$res.list[[1]]$data$DaysLeadTime, y=fcst.adj.eval$res.list[[1]]$ens.mean.angle, col="red")
-      
-      # add title
-      par(oma = c(0, 0, 3, 0))
-      title(main=paste0("Analysis subdaily resolution \n ", fcst$res.list[[1]]$TargetID, "_", fcst$res.list[[1]]$GroupID, "_", fcst$res.list[[1]]$MethodID, "_", fcst$res.list[[1]]$InitYear, "-", fcst$res.list[[1]]$InitDayOfYear), outer = T)
-      
-      dev.off()
     } # end for-loop sub dataset 
 
   } # end for-loop days in res.list
   
 } # end for-loop TargetID
+

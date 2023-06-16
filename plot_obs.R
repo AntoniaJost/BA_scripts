a9c23969f1dd1bd1aecc1beb777650612e6686e5
AntoniaJost <antonia.jost@web.de>
2023-06-16 09:50:00 +0200

    adding daily markers in track

--- plot_obs.R
+++ plot_obs.R
@@ -6,19 +6,19 @@ require(RColorBrewer)
 
 # for whatever reason 300234065498190 cannot be plotted for 2023-80-86 or 80-112
 
-tid = c("900126")
+tids = c("900120", "900121", "900126", "900128", "300234065498190", "300234067527540", "300534063809090", "300534063807110")
 # change tid to tids to use for loop
-# for(i.single in 1:length(tids)){ # quick & dirty way to not enter every tid by hand
-#   tid = tids[i.single]
-#   print(tid)
+for(i.single in 1:length(tids)){ # quick & dirty way to not enter every tid by hand
+  tid = tids[i.single]
+  print(tid)
 
 # set your time preferences
 d.start = 80
 y.start = 2023
-d.end = 112
+d.end = 86
 y.end = 2023
 # I recommend using "arctic" for plotting several buoys and "buoy" for single buoys
-zoom = "buoy" # "buoy"
+zoom = "arctic" # "buoy" or "arctic"
 
 obs = sidfex.read.obs(TargetID = tid)
 
@@ -48,6 +48,17 @@ for(i in 1:length(tid)) {
   lat.obs.all = c(lat.obs, lat.obs.all)
 }
 
+firsty = obs_red$data$Year[1]
+firstdoy = obs_red$data$DOY[1]
+lasty = obs_red$data$Year[length(obs_red$data$Year)]
+lastdoy = obs_red$data$DOY[length(obs_red$data$DOY)]
+daysPassed = sidfex.ydoy2reltime(Year = lasty, DayOfYear = lastdoy, RefYear = firsty, RefDayOfYear = firstdoy)
+days = c(0:daysPassed)
+ydoy = sidfex.reltime2ydoy(reltime = days, RefYear = firsty, RefDayOfYear = firstdoy)
+obs.remap = sidfex.remaptime.obs(obs_red, newtime.YearDayOfYear = ydoy)
+obs.daily = obs.remap
+obs.daily$data = obs.daily$data[-1,] # remove first data point so it doesn't overlap with cross for first day
+
 bc = sl.boundingcircle(lon.obs.all,lat.obs.all) # returns parameters for smallest region which contains every coordinate
 if(zoom == "buoy") {
   # plot centered on bouy
@@ -73,7 +84,7 @@ if(length(tid) == 1) {
   file.name = paste0("obs_", y.start, ":", d.start, "-", d.end, ".png")
 }
 title = paste0("\n", file.name)
-plot.dir = "/home/anjost001/Downloads"#"/home/anjost001/Documents/AWI/Bachelorarbeit/data_analysis/obs"
+plot.dir = "/home/anjost001/Documents/AWI/Bachelorarbeit/data_analysis/obs"
 
 if (file.exists(file.path(plot.dir, file.name))) {
   stop(paste0("File '", file.name, "' already exists. Stopped.")) # skip if file already exists
@@ -103,7 +114,10 @@ if (file.exists(file.path(plot.dir, file.name))) {
     ### plot trajectories
     color = colorRampPalette(brewer.pal(8, "RdYlBu"))(length(tid)) # colorblind-friendly color scale
     sl.plot.lines(pir, lon = lon.obs, lat = lat.obs, col = color[i.obs]) # plot trajectory
-    sl.plot.points(pir,lon = lon.obs[1],lat = lat.obs[1], pch = 4) # plot square at last point of trajectory
+    sl.plot.points(pir,lon = lon.obs[1],lat = lat.obs[1], pch = 4) # plot cross at first point of trajectory
+    if(zoom == "buoy") {
+      sl.plot.points(pir, lon = obs.daily$data$Lon, lat = obs.daily$data$Lat, col = color[i.obs], pch = 16) # point for every new day
+    }
     labelTid = tid[[i.obs]]         # save TargetID to later display in legend
     label = c(labelTid, label)               # save as array, accessible outside of loop
     saveColor = color[i.obs]                 # save color to later display in legend
@@ -112,8 +126,12 @@ if (file.exists(file.path(plot.dir, file.name))) {
   }
   
   sl.plot.end(pir, do.close.device = F) # ending plot before plotting the legend to prevent overlapping
-  legend("bottomright", legend = c(label, "start"), col = c(colorLegend, "black"), pch = c(rep(NA, length(tid)), 4), lty = c(rep(1, length(tid)), 0), bg = "white") # legend
+  if(zoom == "arctic") {
+    legend("bottomright", legend = c(label, "start"), col = c(colorLegend, "black"), pch = c(rep(NA, length(tid)), 4), lty = c(rep(1, length(tid)), 0), bg = "white") # legend
+  } else if(zoom == "buoy") {
+    legend("bottomright", legend = c(label, "new day", "start"), col = c(colorLegend, "red", "black"), pch = c(rep(NA, length(tid)), 16, 4), lty = c(rep(1, length(tid)), 0, 0), bg = "white") # legend
+  }
   
   dev.off()
 }
-#}
+}
